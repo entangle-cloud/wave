5965813770382b7d813230e51bad7a9a54f9a6ea
rukshan <arkruka@gmail.com>
2026-08-24 15:54:42 +0200

    refactor: :recycle: remove sveltekit code to migrate full fastapi backend
    migrating from sveltekit to fastapi backend to seperate client and server code

--- frontend/src/routes/(app)/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-	<div class="aspect-video rounded-xl bg-muted/50"></div>
-	<div class="aspect-video rounded-xl bg-muted/50"></div>
-	<div class="aspect-video rounded-xl bg-muted/50"></div>
-</div>
-<div class="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
