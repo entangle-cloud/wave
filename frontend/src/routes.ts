afcce846a2e2db12f71c0b32a24885350be7ba12
rukshan <arkruka@gmail.com>
2026-08-28 21:26:56 +0200

    refactor: :recycle: application routes

---
+++ frontend/src/routes.ts
@@ -0,0 +1,20 @@
+import Home from './routes/Home.svelte';
+import NotFound from './routes/NotFound.svelte';
+import Editor from "./routes/Editor.svelte"
+import Categories from "./routes/Categories.svelte"
+import CategoryView from "./routes/CategoryView.svelte"
+import Signup from './routes/Signup.svelte';
+import Login from './routes/Login.svelte';
+import { wrap } from "svelte-spa-router/wrap";
+import { requireAuth, skipIfAuthed } from './guard';
+
+export default {
+  '/': wrap({ component: Home, conditions: [requireAuth] }),
+  '/docs': wrap({ component: Editor, conditions: [requireAuth] }),
+  '/docs/:id': wrap({ component: Editor, conditions: [requireAuth] }),
+  '/categories': wrap({ component: Categories, conditions: [requireAuth] }),
+  '/categories/:id': wrap({ component: CategoryView, conditions: [requireAuth] }),
+  '/login': wrap({ component: Login, conditions: [skipIfAuthed] }),
+  '/signup': wrap({ component: Signup, conditions: [skipIfAuthed] }),
+  '*': NotFound,
+};
