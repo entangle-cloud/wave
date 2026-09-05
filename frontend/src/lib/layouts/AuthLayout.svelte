9a28163c76a07ce584e56574e2a1cf7892cbceeb
rukshan <arkruka@gmail.com>
2026-09-05 11:08:15 +0200

    style: :lipstick: add logo and change placeholder name

--- frontend/src/lib/layouts/AuthLayout.svelte
+++ frontend/src/lib/layouts/AuthLayout.svelte
@@ -9,7 +9,10 @@
 
 <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
   <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-lg">
-    <h1 class="font-bold text-center text-2xl mb-5">Your Logo</h1>
+    <div class="flex items-end gap-2 justify-center mb-4">
+      <img src="/wave.png" alt="wave" class="h-24" />
+      <h1 class="font-thin text-center text-2xl mb-5">Wave by Entangle</h1>
+    </div>
     <div
       class="bg-white card shadow w-full rounded-lg divide-y divide-gray-200"
     >
