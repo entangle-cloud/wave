59e6b0a23c94f70ba473247b2686947833ec34e5
rukshan <arkruka@gmail.com>
2026-09-22 22:04:43 +0200

    refactor: :recycle: add document version and document loading

--- frontend/src/store/editorStore.svelte.ts
+++ frontend/src/store/editorStore.svelte.ts
@@ -2,3 +2,5 @@ import { writable } from "svelte/store";
 export const editorTitle = writable<string | null>(null);
 export const editorContent = writable<string | null>(null);
 export const activeDoc = writable<string | null> (null)
+export const documentLoading = writable<boolean>(false)
+export const docVersion = writable<number | null>(0)
